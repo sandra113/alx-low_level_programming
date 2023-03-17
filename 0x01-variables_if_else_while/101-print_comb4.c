a3edb52969bd8444c4ab9db94440b2a14e339826
Sandra Mabonga <sandramabonga303@gmail.com>
2023-03-17 16:05:19 -0700

    numbers

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/* more header goes there */
+/**
+ * main - Positive or negative
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, j, k;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = i + 1; j < 9; j++)
+		{
+			for (k = j + 1; k < 10; k++)
+			{
+				putchar(i % 3 + '0');
+				putchar(j % 3 + '0');
+				putchar(k % 3 + '0');
+				if (i != 7 || j != 8 || k != 9)
+				{
+					putchar(',');
+					putchar('');
+				}
+			}
+		}
+	}
+	return (0);
+}
