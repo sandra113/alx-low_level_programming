3f9cd8279a11b17f375d8213f1840f001b43fb36
Sandra Mabonga <sandramabonga303@gmail.com>
2023-03-17 16:55:04 -0700

    putchar

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,21 +7,15 @@
  */
 int main(void)
 {
-	int i, j;
+	int i;
 
 	for (i = 0; i < 10; i++)
 	{
-		for (j = i + 1; j < 10; j++)
+		putchar(i + '0');
+		if (a < 9)
 		{
-			putchar(i + '0');
 			putchar(',');
 			putchar(' ');
-			putchar(j + '0');
-			if (i != 8 || j != 9)
-			{
-				putchar(',');
-				putchar(' ');
-			}
 		}
 	}
 
