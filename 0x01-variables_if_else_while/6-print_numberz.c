1c02d1a42c8e64f47a263bead2bd0d6d4aa2f648
Sandra Mabonga <sandramabonga303@gmail.com>
2023-03-17 14:34:03 -0700

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int digit = 0;
+
+	while (digit < 10)
+	{
+		putchar(digit % 10 + 48);
+		digit++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
