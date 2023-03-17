75f9a04d9241a1d1aa0766e6201b46473d091034
Sandra Mabonga <sandramabonga303@gmail.com>
2023-03-17 16:38:14 -0700

    combs

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/* more header goes there */
+/**
+ * main - Positive or negative
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i < 100; i++)
+	{
+		for (j = i; j < 100; j++)
+		{
+			if (i != j)
+			{
+				putchar(i / 10 + '0');
+				putchar(i % 10 + '0');
+				putchar(' ');
+				putchar(j / 10 + '0');
+				putchar(j % 10 + '0');
+				if (i != 98 || j != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	return (0);
+}
+
