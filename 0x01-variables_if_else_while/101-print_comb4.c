6546003e2bcc56da4248487006f58493f144ac78
Sandra Mabonga <sandramabonga303@gmail.com>
2023-03-20 02:28:18 -0700

    Success combination

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,20 +8,23 @@ int main(void)
 {
 	int i, j, k;
 
-	for (i = 0; i < 8; i++)
-{
-		for (j = i + 1; j < 9; j++)
+	for (i = '0'; i <= '7'; i++)
+	{
+		for (j = i + 1; j <= '8'; j++)
 		{
-			for (k = j + 1; k < 10; k++)
+			for (k = j + 1; k <= '9'; k++)
 			{
-				putchar(i % 3 + '0');
-				putchar(j % 3 + '0');
-				putchar(k % 3 + '0');
-				if (i != 7 || j != 8 || k != 9)
+				putchar(i);
+				putchar(j);
+				putchar(k);
+				if (i == '7' && j == '8' && k == '9')
+					putchar('\n');
+				else
 				{
 					putchar(',');
 					putchar(' ');
 				}
+
 			}
 		}
 	}
