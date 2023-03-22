40021fc5d9c06820016b0ebe53f241a7d3460956
Sandra Mabonga <sandramabonga303@gmail.com>
2023-03-21 19:07:45 -0700

    98 fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -11,7 +11,7 @@ int main(void)
 	unsigned long fib1 = 0, fib2 = 1, sum;
 	unsigned long fib1_half1, fib1_half2, fib2_half1, fib2_half2;
 	unsigned long half1, half2;
-
+	
 	for (count = 0; count < 92; count++)
 	{
 		sum = fib1 + fib2;
@@ -40,6 +40,6 @@ int main(void)
 		fib2_half1 = half1;
 		fib2_half2 = half2;
 	}
-	print("\n");
+	printf("\n");
 	return (0);
 }
