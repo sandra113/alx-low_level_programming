01981db462e4fa8c50f78f0ecfa8f640a2ca70fb
Sandra Mabonga <sandramabonga303@gmail.com>
2023-03-23 12:40:24 -0700

    square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -9,12 +9,19 @@ void print_square(int size)
 {
 	int x, y;
 
-	for (x = 1; x <= size; x++)
+	if (size <= 0)
 	{
-		for (y = 1; y <= size; y++)
-		{
-			_putchar(35);
-		}
 		_putchar('\n');
+	}
+	else
+	{
+		for (x = 1; x <= size; x++)
+		{
+			for (y = 1; y <= size; y++)
+			{
+				_putchar(35);
+			}
+			_putchar('\n');
+		}
 	}
 }
