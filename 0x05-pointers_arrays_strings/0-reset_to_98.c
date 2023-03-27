913130965e4f269b89641ee234a7bfc6c673bd7e
Sandra Mabonga <sandramabonga303@gmail.com>
2023-03-27 13:59:31 -0700

    update value

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - updates the value to 98
+ * @n: - character to be updated
+ * Return: Always 0.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
