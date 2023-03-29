2d69774f86302f8cc04739fc3d7def01b600d8d7
Sandra Mabonga <sandramabonga303@gmail.com>
2023-03-29 02:50:06 -0700

    kengen

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -3,19 +3,24 @@
 #include <time.h>
 
 #define PASSWORD_LENGTH 10
+/**
+ * main - generates random password
+ *
+ * Return: Always 0.
+ */
 
 int main(void)
 {
-    char password[PASSWORD_LENGTH + 1] = {0};
-    int i;
+	char password[PASSWORD_LENGTH + 1] = {0};
+	int i;
 
-    srand(time(NULL));
+	srand(time(NULL));
 
-    for (i = 0; i < PASSWORD_LENGTH; i++) {
-        password[i] = 'A' + rand() % 26;
-    }
+	for (i = 0; i < PASSWORD_LENGTH; i++)
+	{
+		password[i] = 'A' + rand() % 26;
+	}
 
-    printf("Generated Password: %s\n", password);
-
-    return 0;
+	printf("Generated Password: %s\n", password);
+	return (0);
 }
