bfbcbd77eb1f562a0cc7e6f85ab57ab86b913446
Sandra Mabonga <sandramabonga303@gmail.com>
2023-03-29 02:39:18 -0700

    kengen

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define PASSWORD_LENGTH 10
+
+int main(void)
+{
+    char password[PASSWORD_LENGTH + 1] = {0};
+    int i;
+
+    srand(time(NULL));
+
+    for (i = 0; i < PASSWORD_LENGTH; i++) {
+        password[i] = 'A' + rand() % 26;
+    }
+
+    printf("Generated Password: %s\n", password);
+
+    return 0;
+}
