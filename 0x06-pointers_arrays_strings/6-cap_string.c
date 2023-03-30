57ed31c8eb64e0922061ec917b6a28b766223ad5
Sandra Mabonga <sandramabonga303@gmail.com>
2023-03-29 18:47:23 -0700

    capitalizing first letter of every word

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all words of a string
+ * @str: string
+ * Return: results
+ */
+
+char *cap_string(char *str)
+{
+	int i;
+
+	for (i = 0; str[i]; i++)
+	{
+		if (i == 0 || str[i - 1] == ' ' || str[i - 1] == '\t' || str[i - 1] == '\n')
+		{
+			if (str[i] >= 'a' & str[i] <= 'z')
+			{
+				str[i] -= 'a' - 'A';
+			}
+		}
+	}
+	return (str);
+}
