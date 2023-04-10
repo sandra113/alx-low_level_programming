560f49a14cbce951a0a485ae7b80c7bfc4a6aa2a
Sandra Mabonga <sandramabonga303@gmail.com>
2023-04-10 08:12:46 -0700

    prints file_name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,11 +4,11 @@
 /**
  * main - prints the name of the program
  * @argc: - counts the number of arguments passed through it.
- *@argv: - arguuments passed
+ *@argv: - arguments passed
  *Return: - 0
  */
 int main(int argc __attribute__((unused)), char *argv[])
 {
-	printf("%s\n", argv[0]);
+	printf("%s\n", *argv);
 	return (0);
 }
