631579d90d16a7a4bfb72d9b13b108d8eb924672
Sandra Mabonga <sandramabonga303@gmail.com>
2023-04-11 16:48:41 -0700

    concatanting string arguments

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -18,13 +18,13 @@ char *argstostr(int ac, char **av)
 		return (NULL);
 	for (i = 0; i < ac; i++)
 	{
-		for (j = 0; av[i][j] != '\0'; j++)
+		for (j = 0; av[i][j]; j++)
 		{
 			len++; /*total length of the concatenated string*/
 		}
 		len += ac; /*for each new line character add 1*/
 	}
-	str = malloc(sizeof(char) * (len + 1));
+	str =(char *)malloc(sizeof(char) * (len + 1));
 	if (str == NULL)
 		return (NULL);
 
