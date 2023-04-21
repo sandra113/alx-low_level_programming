aa08a1ad23c2cdfca3539e9c782c7796251a0244
Sandra Mabonga <sandramabonga303@gmail.com>
2023-04-20 18:18:04 -0700

    print strings

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -23,7 +23,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 		if (string == NULL || string[0] == '\0')
 		{
-			printf("nil\n");
+			printf("(nil)\n");
 		}
 		printf("%s", string);
 		if (i < n - 1)
