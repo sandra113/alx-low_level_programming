8d8566bb2bd86708949b4c11359368d5795448b4
Sandra Mabonga <sandramabonga303@gmail.com>
2023-04-27 16:34:05 -0700

    singly list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,20 +8,23 @@
  * @h: elements of list_t
  * Return: the number of nodes
  */
+
 size_t print_list(const list_t *h)
 {
 	size_t i;
 
 	i = 0;
 
-	if (h->str == NULL)
+	while (h)
 	{
-		printf("[0](nil)\n");
-	}
-
-	while (h != NULL)
-	{
-		printf("[%u]%s\n", h->len, h->str);
+		if (h->str == NULL)
+		{
+			printf("[0](nil)\n");
+		}
+		else
+		{
+			printf("[%u]%s\n", h->len, h->str);
+		}
 		h = h->next;
 		i++;
 	}
