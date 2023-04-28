1a46c752a8b2a3a15c231a0bd71f1cb8f538d229
Sandra Mabonga <sandramabonga303@gmail.com>
2023-04-27 18:04:21 -0700

    add element

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -12,20 +12,18 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
-	unsigned int len2;
-
-	for (len2 = 1; str[len2] != '\0'; len2++)
 
 	new_node = malloc(sizeof(list_t));
 	if (!new_node)
 		return (NULL);
+
 	new_node->str = strdup(str);
 	if (new_node->str == NULL)
 	{
 		free(new_node);
 		return (NULL);
 	}
-	new_node->len = len2;
+	new_node->len = strlen(str);
 	new_node->next = (*head);
 
 	(*head) = new_node;
