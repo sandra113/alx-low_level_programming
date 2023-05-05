bdfc5ae72beefee9d9a301095eeb141606c65188
Sandra Mabonga <sandramabonga303@gmail.com>
2023-05-04 18:39:20 -0700

    get endian

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * get_endianness - checks the enduanes
+ * Returns: 0 (big endian), 1 (little endian)
+ */
+
+int get_endianness(void)
+{
+	int n = 1;
+	char *p;
+
+	/*check the first byte of the test value*/
+	p = (char *)&n;
+
+	/*it is little endian if the first byte is 1*/
+
+	if (*p == 1)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
