4ba85dd29428cb782c85c0f896aaaa5a4eafdefc
root <sandramabonga303@gmail.com>
2023-07-06 18:35:51 -0700

    set bit

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -8,19 +8,18 @@
  * Return: the new value
  */
 
-int get_bit(unsigned long int n, unsigned int index)
+int set_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int mask;
-	int bit_value;
 
-	if (index >= sizeof(unsigned long int) * 8)
+	if (index >= sizeof(unsigned long int) * 8 || n == NULL)
 	{
-		printf("Error: Invalid bit index\n");
+		printf("Error: Invalid bit index or NULL pointer\n");
 		return (-1);
 	}
 
 	mask = 1UL << index;
-	bit_value = (n & mask) != 0;
+	*n |= mask;
 
-	return (bit_value);
+	return (1);
 }
