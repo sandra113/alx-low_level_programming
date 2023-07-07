211089f2db19d76283e892b89e789c4e9a5a26a3
root <sandramabonga303@gmail.com>
2023-07-06 18:28:12 -0700

    get bit

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * get_bit - sets the value of a bit to 1 at a given index.
+ * @n: the number
+ * @index: the index of the number
+ * Return: the new value
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int mask;
+	int bit_value;
+
+	if (index >= sizeof(unsigned long int) * 8)
+	{
+		printf("Error: Invalid bit index\n");
+		return (-1);
+	}
+
+	mask = 1UL << index;
+	bit_value = (n & mask) != 0;
+
+	return (bit_value);
+}
