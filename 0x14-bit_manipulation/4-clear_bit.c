18cbf66bc9a08fac27df634262ff8c184f42edb2
root <sandramabonga303@gmail.com>
2023-07-06 18:43:08 -0700

    clear bit

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * clear_bit - sets the value of a bit to 1 at a given index.
+ * @n: the number
+ * @index: the index of the number
+ * Return: the new value
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int mask;
+
+	if (index >= sizeof(unsigned long int) * 8 || n == NULL)
+	{
+		printf("Error: Invalid bit index or NULL pointer\n");
+		return (-1);
+	}
+
+	mask = 1UL << index;
+	*n &= ~mask;
+
+	return (1);
+}
