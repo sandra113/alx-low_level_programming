a24475437feb4e22b98689f73d87df27ffa36fa2
root <sandramabonga303@gmail.com>
2023-07-10 16:47:15 -0700

    correction on malloc

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -22,7 +22,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (file == NULL)
 		return (0);
 
-	buf = malloc(sizeof(char) * (letters + 1));
+	buf = malloc(sizeof(char) * (letters));
 	if (buf == NULL)
 		return (0);
 
