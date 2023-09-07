aaa12dda75715a94e66f394e2fe3770732c144ae
root <sandramabonga303@gmail.com>
2023-09-07 12:29:31 -0700

    djb2 algorithm

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,12 +9,19 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	unsigned long int i;
-	hash_table_t *table = (hash_table_t*) malloc(sizeof(hash_table_t));
+
+	hash_table_t *table = (hash_table_t *) malloc(sizeof(hash_table_t));
+	if (table == NULL)
+		return (NULL)
+
+	table->array = (hash_node_t **) calloc(table->size, sizeof(hash_node_t));
+	if (table->array == NULL)
+		return (NULL)
+
 	table->size = size;
-	table->array = (hash_node_t**) calloc(table->size, sizeof(hash_node_t));
 
 	for (i = 0; i < table->size; i++)
 		table->array[i] = NULL;
 
-	return(table);
+	return (table);
 }
---
+++ 0x1A-hash_tables/1-djb2.c
@@ -0,0 +1,19 @@
+#include "hash_tables.h"
+
+/**
+ * hash_djb2 - implements the djb2 algorithm
+ * @str: a pointer to the beginning of a string
+ * Return: the final hash value
+ */
+unsigned long int hash_djb2(const unsigned char *str)
+{
+	unsigned long int hash = 5381;
+	int c;
+	
+	while (*str)
+	{
+		c = *str++;
+		hash = (hash * 33) ^ c;
+	}
+	return (hash);
+}
