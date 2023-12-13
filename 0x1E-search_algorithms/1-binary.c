9903b6b0992ec3cd6e80eb23e70640cf6ab0a769
root <sandramabonga303@gmail.com>
2023-12-13 10:32:38 -0800

    binary search

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -22,7 +22,7 @@ int binary_search(int *array, size_t size, int value)
 	while (left <= right)
 	{
 		mid = left + (right - left) / 2;
-		printf("Searching in subarray: ");
+		printf("Searching in array: ");
 		for (i = left; i <= right; ++i)
 		{
 			if (i != right)
